f0a461146ea247c55e6f5912e615cd5e809a5850
Isabela Caserta <casertaisabela@gmail.com>
2023-10-03 10:36:44 -0300

    Initial Commit

---
+++ Motif/MotifApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotifApp.swift
+//  Motif
+//
+//  Created by Isabela Caserta on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MotifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
